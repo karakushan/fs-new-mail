1f721fe156aa0050d14c0cd6b818e6e2ed0290fc
Vitaliy <karakushan@gmail.com>
2025-03-26 12:05:24 +0200

    додав обробку помилок при пошуку міста

--- inc/class-fs-new-mail.php
+++ inc/class-fs-new-mail.php
@@ -2,243 +2,270 @@
 
 class FS_New_Mail
 {
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     *
+     * @var string the ID of this plugin
+     */
+    private $plugin_name;
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string $plugin_name The ID of this plugin.
-	 */
-	private $plugin_name;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     *
+     * @var string the current version of this plugin
+     */
+    private $version;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string $version The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * API РєР»СЋС‡ РќРѕРІР°СЏ РџРѕС‡С‚Р°.
+     *
+     * @since    1.0.0
+     *
+     * @var string
+     */
+    private $api_key;
 
+    /**
+     * РўРѕС‡РєР° РІС…РѕРґР° РґР»СЏ API РќРѕРІР°СЏ РџРѕС‡С‚Р°.
+     *
+     * @since    1.0.0
+     *
+     * @var string
+     */
+    private $api_host = 'https://api.novaposhta.ua/v2.0/json/';
 
-	/**
-	 * API РєР»СЋС‡ РќРѕРІР°СЏ РџРѕС‡С‚Р°.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string
-	 */
-	private $api_key;
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     */
+    public function __construct()
+    {
+        // присваиваем API ключ из настроек
+        $this->api_key = fs_option('nm_api_key');
 
+        /*
+         * Ajax запрос для получения городов Новой Почты.
+         */
+        add_action('wp_ajax_fs_get_city', [$this, 'fs_get_city']);
+        add_action('wp_ajax_nopriv_fs_get_city', [$this, 'fs_get_city']);
 
-	/**
-	 * РўРѕС‡РєР° РІС…РѕРґР° РґР»СЏ API РќРѕРІР°СЏ РџРѕС‡С‚Р°.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string
-	 */
-	private $api_host = 'https://api.novaposhta.ua/v2.0/json/';
+        /*
+         * Ajax запрос для получения отделений Новой Почты.
+         */
+        add_action('wp_ajax_fs_get_warehouses', [$this, 'fs_get_warehouses']);
+        add_action('wp_ajax_nopriv_fs_get_warehouses', [$this, 'fs_get_warehouses']);
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @param string $plugin_name The name of the plugin.
-	 * @param string $version The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 *
-	 */
-	public function __construct()
-	{
-		// присваиваем API ключ из настроек
-		$this->api_key = fs_option('nm_api_key');
+        // Получаем ID города по переданому названию
+        add_action('wp_ajax_fs_nm_city_code', [$this, 'fs_nm_city_code']);
+        add_action('wp_ajax_nopriv_fs_nm_city_code', [$this, 'fs_nm_city_code']);
 
-		/**
-		 * Ajax запрос для получения городов Новой Почты.
-		 */
-		add_action('wp_ajax_fs_get_city', array($this, 'fs_get_city'));
-		add_action('wp_ajax_nopriv_fs_get_city', array($this, 'fs_get_city'));
+        /*
+         * Добавляем поля в профиль пользователя.
+         */
+        add_filter('fs_user_fields', [$this, 'fs_user_fields']);
+    }
 
-		/**
-		 * Ajax запрос для получения отделений Новой Почты.
-		 */
-		add_action('wp_ajax_fs_get_warehouses', array($this, 'fs_get_warehouses'));
-		add_action('wp_ajax_nopriv_fs_get_warehouses', array($this, 'fs_get_warehouses'));
+    public function fs_user_fields($fields)
+    {
+        $fields['fs_region_ref'] = [
+            'type' => 'text',
+            'name' => __('Код регіону (Новая Пошта)', 'fs-new-mail'),
+            'label' => '',
+            'value' => get_user_meta(get_current_user_id(), 'fs_region_ref', true),
+        ];
 
-		// Получаем ID города по переданому названию
-		add_action('wp_ajax_fs_nm_city_code', array($this, 'fs_nm_city_code'));
-		add_action('wp_ajax_nopriv_fs_nm_city_code', array($this, 'fs_nm_city_code'));
+        $fields['fs_city_ref'] = [
+            'type' => 'text',
+            'name' => __('Код міста (Нова Пошта)', 'fs-new-mail'),
+            'label' => '',
+            'value' => get_user_meta(get_current_user_id(), 'fs_city_ref', true),
+        ];
 
-		/**
-		 * Добавляем поля в профиль пользователя.
-		 */
-		add_filter('fs_user_fields', array($this, 'fs_user_fields'));
-	}
+        return $fields;
+    }
 
-	public function fs_user_fields($fields)
-	{
-		$fields['fs_region_ref'] = array(
-			'type' => 'text',
-			'name' => __('Код регіону (Новая Пошта)', 'fs-new-mail'),
-			'label' => '',
-			'value' => get_user_meta(get_current_user_id(), 'fs_region_ref', true),
-		);
+    /**
+     * отправляет запрос методом POST.
+     *
+     * @param array $args
+     *
+     * @return bool|object
+     */
+    public function send_query($args)
+    {
+        $args['apiKey'] = $this->api_key;
 
-		$fields['fs_city_ref'] = array(
-			'type' => 'text',
-			'name' => __('Код міста (Нова Пошта)', 'fs-new-mail'),
-			'label' => '',
-			'value' => get_user_meta(get_current_user_id(), 'fs_city_ref', true),
-		);
+        // преобразование в JSON-формат
+        $request = json_encode($args);
 
-		return $fields;
-	}
-	/**
-	 * отправляет запрос методом POST
-	 *
-	 * @param array $args
-	 *
-	 * @return bool
-	 */
-	function send_query($args)
-	{
-		$args["apiKey"] = $this->api_key;
+        // параметры запроса
+        $opts = [
+            'http' => [
+                'method' => 'POST',
+                'header' => "Content-type: application/x-www-form-urlencoded;\r\n",
+                'content' => $request,
+            ],
+        ];
 
-		# преобразование в JSON-формат
-		$request = json_encode($args);
+        // создание контекста потока
+        $context = stream_context_create($opts);
 
-		# параметры запроса
-		$opts = [
-			'http' => [
-				'method'  => "POST",
-				'header'  => "Content-type: application/x-www-form-urlencoded;\r\n",
-				'content' => $request,
-			]
-		];
+        // отправляем запрос и получаем ответ от сервера
+        $result = file_get_contents($this->api_host, 0, $context);
+        $res = json_decode($result);
+        if ($res->success) {
+            return $res->data;
+        } else {
+            return (object) [
+                'success' => false,
+                'errors' => !empty($res->errors) ? $res->errors : [__('Неизвестная ошибка API', 'fs-new-mail')],
+                'errorCodes' => !empty($res->errorCodes) ? $res->errorCodes : [],
+                'data' => null,
+            ];
+        }
+    }
 
-		# создание контекста потока
-		$context = stream_context_create($opts);
+    // Получаем ID города по переданому названию
+    public function fs_nm_city_code()
+    {
+        $query = [
+            'modelName' => 'Address',
+            'calledMethod' => 'searchSettlements',
+            'methodProperties' => [
+                'CityName' => sanitize_text_field($_POST['input']),
+            ],
+        ];
+        $response = $this->send_query($query);
 
-		# отправляем запрос и получаем ответ от сервера
-		$result = file_get_contents($this->api_host, 0, $context);
-		$res    = json_decode($result);
-		if ($res->success) {
-			return $res->data;
-		} else {
-			return $res;
-		}
-	}
+        // Проверяем на ошибки
+        if (is_object($response) && isset($response->success) && $response->success === false) {
+            wp_send_json_error([
+                'msg' => implode(', ', $response->errors),
+                'errorCodes' => $response->errorCodes,
+            ]);
 
-	// Получаем ID города по переданому названию
-	function fs_nm_city_code()
-	{
-		$query    = [
-			"modelName"        => "Address",
-			"calledMethod"     => "searchSettlements",
-			"methodProperties" => [
-				"CityName" => sanitize_text_field($_POST['input']),
-			]
-		];
-		$response = $this->send_query($query);
-		/*print_r( $response[0]->TotalCount );
-		wp_die();*/
-		if (! empty($response[0]->TotalCount) && ! empty($response[0]->Addresses)) {
-			$out = '<select name="select-from-nm-city" style="display: block;">';
-			$out .= '<option value="">' . esc_html__('Виберіть населений пункт', 'fs-new-mail') . '</option>';
-			foreach ($response[0]->Addresses as $address) {
-				$out .= '<option value="' . esc_attr($address->DeliveryCity) . '">' . esc_html($address->Present) . '</option>';
-			}
-			$out .= '</select>';
-			wp_send_json_success(['html' => $out]);
-		}
+            return;
+        }
 
-		wp_send_json_error();
-	}
+        // Проверяем, является ли $response объектом stdClass
+        if (is_object($response) && property_exists($response, 'data')) {
+            $response = [$response->data];
+        }
 
+        if (!empty($response[0]->TotalCount) && !empty($response[0]->Addresses)) {
+            $out = '<select name="select-from-nm-city" style="display: block;">';
+            $out .= '<option value="">'.esc_html__('Виберіть населений пункт', 'fs-new-mail').'</option>';
+            foreach ($response[0]->Addresses as $address) {
+                $out .= '<option value="'.esc_attr($address->DeliveryCity).'">'.esc_html($address->Present).'</option>';
+            }
+            $out .= '</select>';
+            wp_send_json_success(['html' => $out]);
+        }
 
-	/**
-	 * метод-колбек для ajax запроса по получения городов
-	 * https://devcenter.novaposhta.ua/docs/services/556d7ccaa0fe4f08e8f7ce43/operations/58e5ebeceea27017bc851d67
-	 */
-	public function fs_get_city()
-	{
-		$query    = [
-			"modelName"        => "Address",
-			"calledMethod"     => "searchSettlements",
-			"methodProperties" => [
-				"CityName" => sanitize_text_field($_POST['cityName']),
-			]
-		];
-		$response = $this->send_query($query);
-		//	wp_send_json($response);
+        wp_send_json_error(['msg' => __('Не знайдено населених пунктів', 'fs-new-mail')]);
+    }
 
-		if (! empty($response[0]->Addresses)) {
-			$cities = $response[0]->Addresses ?? [];
-			$cities = array_map(function ($item) {
-				return [
-					'name' => $item->Present,
-					'ref'  => $item->DeliveryCity
-				];
-			}, $cities);
-			wp_send_json_success(compact('cities'));
-		} else {
-			wp_send_json_error(array('msg' => __('не знайдено віділень Нової Пошти', 'fs-new-mail')));
-		}
-	}
+    /**
+     * метод-колбек для ajax запроса по получения городов
+     * https://devcenter.novaposhta.ua/docs/services/556d7ccaa0fe4f08e8f7ce43/operations/58e5ebeceea27017bc851d67.
+     */
+    public function fs_get_city()
+    {
+        $query = [
+            'modelName' => 'AddressGeneral',
+            'calledMethod' => 'searchSettlements',
+            'methodProperties' => [
+                'CityName' => sanitize_text_field($_POST['cityName']),
+                'Limit' => isset($_POST['limit']) ? intval($_POST['limit']) : 50,
+                'Page' => isset($_POST['page']) ? intval($_POST['page']) : 1,
+            ],
+        ];
+        $response = $this->send_query($query);
 
-	/**
-	 * метод-колбек для ajax запроса по получения отделений в городе
-	 *
-	 * https://devcenter.novaposhta.ua/docs/services/556d7ccaa0fe4f08e8f7ce43/operations/556d8211a0fe4f08e8f7ce45
-	 */
-	public function fs_get_warehouses()
-	{
-		$query    = [
-			"modelName"        => "AddressGeneral",
-			"calledMethod"     => "getWarehouses",
-			"methodProperties" => [
-				//				"CityName" => sanitize_text_field( $_POST['cityName'] ),
-				"CityRef"  => sanitize_text_field($_POST['ref'])
+        // Проверяем на ошибки
+        if (is_object($response) && isset($response->success) && $response->success === false) {
+            wp_send_json_error([
+                'msg' => implode(', ', $response->errors),
+                'errorCodes' => $response->errorCodes,
+            ]);
 
-			]
-		];
-		$response = $this->send_query($query);
+            return;
+        }
 
-		$delivery_cost = 0;
-		if (fs_option('fs_nm_from_city_id')) {
-			$response_cost_delivery = $this->send_query(array(
-				"modelName"        => "InternetDocument",
-				"calledMethod"     => "getDocumentPrice",
-				"methodProperties" => [
-					"CitySender"    => sanitize_text_field(fs_option('fs_nm_from_city_id')),
-					"CityRecipient" => sanitize_text_field($_POST['ref']),
-					"Weight"        => 2
-				]
-			));
-			if (! empty($response_cost_delivery[0]->CostWarehouseWarehouse)) {
-				$delivery_cost = floatval($response_cost_delivery[0]->CostWarehouseWarehouse);
-			}
-		}
+        // Проверяем, является ли $response объектом stdClass
+        if (is_object($response) && property_exists($response, 'data')) {
+            $response = [$response->data];
+        }
 
-		if (! empty($response)) {
-			$warehouses = array_map(function ($item) {
-				return [
-					'description' => $item->Description,
-					'ref'         => $item->Ref
-				];
-			}, $response);
+        if (!empty($response[0]->Addresses)) {
+            $cities = $response[0]->Addresses ?? [];
+            $cities = array_map(function ($item) {
+                return [
+                    'name' => $item->Present,
+                    'ref' => $item->DeliveryCity,
+                ];
+            }, $cities);
+            wp_send_json_success(compact('cities'));
+        } else {
+            wp_send_json_error(['msg' => __('не знайдено віділень Нової Пошти', 'fs-new-mail')]);
+        }
+    }
 
-			wp_send_json_success(array(
-				'warehouses'   => $warehouses,
-				'deliveryCost' => sprintf('%s <span>%s</span>', apply_filters('fs_price_format', $delivery_cost), fs_currency()),
-				'totalAmount'  => sprintf('%s <span>%s</span>', apply_filters('fs_price_format', fs_get_total_amount($delivery_cost)), fs_currency())
-			));
-		}
+    /**
+     * метод-колбек для ajax запроса по получения отделений в городе.
+     *
+     * https://devcenter.novaposhta.ua/docs/services/556d7ccaa0fe4f08e8f7ce43/operations/556d8211a0fe4f08e8f7ce45
+     */
+    public function fs_get_warehouses()
+    {
+        $query = [
+            'modelName' => 'AddressGeneral',
+            'calledMethod' => 'getWarehouses',
+            'methodProperties' => [
+                //				"CityName" => sanitize_text_field( $_POST['cityName'] ),
+                'CityRef' => sanitize_text_field($_POST['ref']),
+            ],
+        ];
+        $response = $this->send_query($query);
 
-		wp_send_json_error(array(
-			'msg' => __('Не знайдено відділень, виберіть найближче до вас місто')
-		));
-	}
+        $delivery_cost = 0;
+        if (fs_option('fs_nm_from_city_id')) {
+            $response_cost_delivery = $this->send_query([
+                'modelName' => 'InternetDocument',
+                'calledMethod' => 'getDocumentPrice',
+                'methodProperties' => [
+                    'CitySender' => sanitize_text_field(fs_option('fs_nm_from_city_id')),
+                    'CityRecipient' => sanitize_text_field($_POST['ref']),
+                    'Weight' => 2,
+                ],
+            ]);
+            if (!empty($response_cost_delivery[0]->CostWarehouseWarehouse)) {
+                $delivery_cost = floatval($response_cost_delivery[0]->CostWarehouseWarehouse);
+            }
+        }
+
+        if (!empty($response) && is_array($response)) {
+            $warehouses = array_map(function ($item) {
+                return [
+                    'description' => $item->Description,
+                    'ref' => $item->Ref,
+                ];
+            }, $response);
+
+            wp_send_json_success([
+                'warehouses' => $warehouses,
+                'deliveryCost' => sprintf('%s <span>%s</span>', apply_filters('fs_price_format', $delivery_cost), fs_currency()),
+                'totalAmount' => sprintf('%s <span>%s</span>', apply_filters('fs_price_format', fs_get_total_amount($delivery_cost)), fs_currency()),
+            ]);
+        }
+
+        wp_send_json_error([
+            'msg' => __('Не знайдено відділень, виберіть найближче до вас місто'),
+        ]);
+    }
 }
